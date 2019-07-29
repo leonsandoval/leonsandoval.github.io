07375bb5129456ddf71aba665f3a1ad768f6f493
leonsandoval <lasandov@uc.cl>
2019-07-29 15:01:25 -0400

    descargar txt

--- scripts/querys.js
+++ scripts/querys.js
@@ -239,3 +239,19 @@ function escribirmass() {
 }
 
 
+function downloadInnerHtml(filename, elId, mimeType) {
+    var elHtml = document.getElementById(elId).innerHTML;
+    var link = document.createElement('a');
+    mimeType = mimeType || 'text/plain';
+
+    link.setAttribute('download', filename);
+    link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(elHtml));
+    link.click();
+}
+
+var fileName = 'tags.txt'; // You can use the .txt extension if you want
+
+$('#downloadLink').click(function () {
+    downloadInnerHtml(fileName, 'escribe', 'text/html');
+});
+
